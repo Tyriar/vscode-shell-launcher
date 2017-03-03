0e4e45e396ec480e5bea6f929f26ca76e4250da9
Daniel Imms <daimms@microsoft.com>
2017-03-03 11:34:41 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Shell Launcher
+
+Easily launch multiple shell configurations in the terminal.
+
+![Shell launcher in action](images/quick-pick.png)
+
+## Commands
+
+This extension exposes the following commands that can be keybound in your [keybindings.json](https://code.visualstudio.com/docs/customization/keybindings#_customizing-shortcuts) file:
+
+- `shellLauncher.launch`: Display a quick pick that allows selecting of one of the configured terminal shells.
+
+## Settings
+
+All shells can be user configured in your [settings.json](https://code.visualstudio.com/docs/customization/userandworkspace) file, there is a different key for each platform:
+
+- `shellLauncher.shells.linux`: For Linux
+- `shellLauncher.shells.osx`: For macOS
+- `shellLauncher.shells.windows`: For Window
+
+Here is an example shell which will launch `bash` as a login shell (`bash -l`) on Linux:
+
+```js
+"shellLauncher.shells.linux": [
+{
+  "shell": "bash",
+  "args": ["-l"],
+  "label": "bash login shell"
+},
+```
---
+++ src/extension.ts
@@ -0,0 +1,68 @@
+'use strict';
+
+import * as os from 'os';
+import * as vscode from 'vscode';
+
+interface ShellConfig {
+    shell: string;
+    args?: string[];
+    label?: string;
+}
+
+interface ShellLauncherConfig {
+    shells: {
+        linux: ShellConfig[];
+        osx: ShellConfig[];
+        windows: ShellConfig[];
+    };
+}
+
+function getShells(): ShellConfig[] {
+    const config = <ShellLauncherConfig>vscode.workspace.getConfiguration().get('shellLauncher');
+    const shells = config.shells;
+    if (os.platform() === 'win32') {
+        return shells.windows;
+    }
+    if (os.platform() === 'darwin') {
+        return shells.osx;
+    }
+    return shells.linux;
+}
+
+function getShellLabel(shell: ShellConfig) {
+    if (shell.label) {
+        return shell.label;
+    }
+    return getShellDescription(shell);
+}
+
+function getShellDescription(shell: ShellConfig) {
+    if (!shell.args || shell.args.length === 0) {
+        return shell.shell;
+    }
+    return `${shell.shell} ${shell.args.join(' ')}`;
+}
+
+export function activate(context: vscode.ExtensionContext) {
+    const disposable = vscode.commands.registerCommand('shellLauncher.launch', () => {
+        const shells = getShells();
+        const options: vscode.QuickPickOptions = {
+            placeHolder: 'Select the shell to launch'
+        }
+        const items: vscode.QuickPickItem[] = shells.map(s => {
+            return {
+                label: getShellLabel(s),
+                description: getShellDescription(s)
+            };
+        });
+        vscode.window.showQuickPick(items, options).then(item => {
+            const shell = shells.filter(c => getShellLabel(c) === item.label)[0];
+            const terminal = vscode.window.createTerminal(undefined, shell.shell, shell.args);
+            terminal.show();
+        });
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+export function deactivate() { }
