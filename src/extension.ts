54efd880b5eafe7d095f810880e13e702891a29f
Jared Miller <1668550+Jared-Miller@users.noreply.github.com>
2017-11-23 04:27:22 +0000

    Add ability to name terminals (#3)

--- src/extension.ts
+++ src/extension.ts
@@ -7,6 +7,7 @@ interface ShellConfig {
     shell: string;
     args?: string[];
     label?: string;
+    launchNamed?: string;
 }
 
 interface ShellLauncherConfig {
@@ -57,7 +58,7 @@ export function activate(context: vscode.ExtensionContext) {
         });
         vscode.window.showQuickPick(items, options).then(item => {
             const shell = shells.filter(c => getShellLabel(c) === item.label)[0];
-            const terminal = vscode.window.createTerminal(undefined, shell.shell, shell.args);
+            const terminal = vscode.window.createTerminal(shell.launchNamed, shell.shell, shell.args);
             terminal.show();
         });
     });
