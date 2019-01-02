22d03e32c369b2ee14be65d758c0b401b7308e33
Daniel Imms <daimms@microsoft.com>
2019-01-02 07:54:54 -0800

    Simplify testing

---
+++ src/extension.test.ts
@@ -0,0 +1,8 @@
+import * as assert from 'assert';
+
+describe("Extension Tests", () => {
+    it("Something 1", () => {
+        assert.equal(-1, [1, 2, 3].indexOf(5));
+        assert.equal(-1, [1, 2, 3].indexOf(0));
+    });
+});
